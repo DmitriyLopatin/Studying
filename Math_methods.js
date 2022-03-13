0ae3f4462c969c4dae1d720d1cb7d61853542cc5
DmitriyLopatin <97829709+DmitriyLopatin@users.noreply.github.com>
2022-03-13 13:22:48 +0600

    Add files via upload

---
+++ Math_methods.js
@@ -0,0 +1,8 @@
+//-----------Произвольное число-----------------------
+console.log(Math.floor(Math.random()*25)) 
+
+// ------нахождение Мак и Мин числа в массиве-------
+// let z = [6,6,6]
+// console.log(Math.max.apply(null,z))
+// console.log(Math.min.apply(null,z))
+
