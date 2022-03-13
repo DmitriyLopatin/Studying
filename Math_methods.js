288943cd549f6e90b76b5b412b69ffd1ffe03aac
DmitriyLopatin <97829709+DmitriyLopatin@users.noreply.github.com>
2022-03-13 13:40:50 +0600

    Update Math_methods.js

--- Math_methods.js
+++ Math_methods.js
@@ -1,8 +1,10 @@
 //-----------Произвольное число-----------------------
 console.log(Math.floor(Math.random()*25)) 
 
-// ------нахождение Мак и Мин числа в массиве-------
+// ----------нахождение Мак и Мин числа в массиве-------
 // let z = [6,6,6]
 // console.log(Math.max.apply(null,z))
 // console.log(Math.min.apply(null,z))
 
+//----------Math.trunc() возвращает целую часть числа
+//console.log(Math.trunc(13.37));
